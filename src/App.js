7cb56863bcf6a6a5720dcee3b4ab480fe49151bc
francisco28 <fgallegosgo@gmail.com>
2020-05-27 12:42:40 -0600

    styled component in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import Header from './components/Header';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContainerForm = styled.div`
+  background-color: #FFF;
+  padding: 3rem;
+`;
 
 function App() {
   return (
-    <Header 
-      title='Insurance quote'
-    />
+    <Container>
+        <Header 
+        title='Insurance quote'
+        />
+
+        <ContainerForm>
+          
+        </ContainerForm>
+      
+    </Container>
   );
 }
 
